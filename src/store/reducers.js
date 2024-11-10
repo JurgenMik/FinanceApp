1e654152265c0b609b9e1a6c23d9987445420762
Jürgen Mikk <jurgen.mikk@khk.ee>
2024-11-11 00:08:54 +0300

    FS-fixed-deprecated-redux-configuration-approach-and-module-errors

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,1 +1,21 @@
-// Define `state` handlers
+import { combineReducers } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+const reducerPlaceholderA = createSlice({
+  name: 'placeA',
+  initialState: { value: 0 },
+  reducers: {}
+});
+
+const reducerPlaceholderB = createSlice({
+  name: 'placeB',
+  initialState: { value: 0 },
+  reducers: {}
+});
+
+const rootReducer = combineReducers({
+  placeholderA: reducerPlaceholderA.reducer, 
+  placeholderB: reducerPlaceholderB.reducer
+});
+
+export default rootReducer;
